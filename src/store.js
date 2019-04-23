916b80eb70f8b102863a1a5b6c1b19d1375365ab
Alexandr <iamwebdev@gmail.com>
2019-04-23 12:25:09 -0400

    fix to run

--- src/store.js
+++ src/store.js
@@ -17,14 +17,13 @@ const middleware = [
   routerMiddleware(history),
   sagaMiddleware
 ];
-
+/*
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
-
   if (typeof devToolsExtension === 'function') {
     enhancers.push(devToolsExtension());
   }
-}
+}*/
 
 const composedEnhancers = composeWithDevTools(
   applyMiddleware(...middleware),
