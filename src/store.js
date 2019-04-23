128be8830e446b0ca79e16efdf35c6954e584a4c
Alexandr <iamwebdev@gmail.com>
2019-04-23 12:00:38 -0400

    fix remove createBrowserHistory Warning

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducers';
 import sagas from './sagas';
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const sagaMiddleware = createSagaMiddleware();
 
